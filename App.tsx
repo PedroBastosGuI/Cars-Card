365cb26912a296b2ab50095bcb75d4faeddd6f5a
Pedro Guilherme <pedrobastos6540@gmail.com>
2022-02-01 08:59:01 -0300

    theme configuration

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/style/global/theme';
+
 import { Home } from './src/Screens/home';
 
+import {} from '@expo-google-fonts/archivo';
+import {} from '@expo-google-fonts/inter';
+
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+
+
 export default function App() {
+
+  const[fontsloaded] = useFonts({
+
+  })
+
+  if(!fontsloaded){
+    return <AppLoading/>
+  }
+
   return (
   
   <ThemeProvider theme={theme}> 
